9a8993ddb52fbc1ef61c7b77ddf4e1a3f30686df
ninglolo <57176366+ninglolo@users.noreply.github.com>
2022-02-21 17:43:03 +0800

    Update get_essentialLncRNAs.py

--- src/get_essentialLncRNAs.py
+++ src/get_essentialLncRNAs.py
@@ -4,8 +4,8 @@ import csv
 # obtain the degree of each lncRNA
 def getS(filename):
     fLPI = open(filename,'r')
-    lnc_protein = {}
-    s = {}
+    lnc_protein = {} #preserve the interaction between lncRNA and protein
+    s = {} #save the degree of lncRNAs
     
     for line in fLPI:
         line = line.strip()
@@ -30,7 +30,7 @@ def getRank(filename):
     lnc = []
     f = open(filename, 'r')
     i = 0
-    rank = {}
+    rank = {} #Preserve the rank of lncRNAs
     for line in f:
         line = line.strip()
         data = line.split(',')
@@ -44,25 +44,25 @@ def getRank(filename):
 
 # obtain SGII prediction result under different parameter combinations
 def getEssentialLncRNAs(K, T, z):
-    lnc_bigger_z = []
-    lnc_smaller_z = []
+    lnc_bigger_z = []  #Preserve the lncRNAs that the degree is equal to and bigger than z.
+    lnc_smaller_z = [] #Preserve the lncRNAs that the degree is smaller than z.
     for k, v in s.items():
         if v >= z:
             lnc_bigger_z.append(k)
         else:
             lnc_smaller_z.append(k)
 
-    essenLnc_centrality = []
+    essenLnc_centrality = []  #essential lncRNAs predicted by centralities
     for i in lnc_bigger_z:
         if i in r_b and r_b[i] < len(r_b)*K*0.01 and r_c[i] < len(r_c)*K*0.01 and r_d[i] < len(r_d)*K*0.01 and r_e[i] < len(r_e)*K*0.01:
             essenLnc_centrality.append(i)
 
-    essenLnc_GIC = []
+    essenLnc_GIC = [] #essential lncRNAs predicted by GIC
     for i in lnc_smaller_z:
         if i in r_g and r_g[i] < len(r_b)*T*0.01:
             essenLnc_GIC.append(i)
 
-    essenLnc = []
+    essenLnc = [] #essential lncRNAs predicted by SGII
     for i in essenLnc_centrality:
         essenLnc.append(i)
     for i in essenLnc_GIC:
@@ -73,9 +73,9 @@ def getEssentialLncRNAs(K, T, z):
 
 if __name__ == '__main__':
     
-    dataPath1 = '../data/'+ sys.argv[1] + '/'
-    dataPath2 = '../result/'+ sys.argv[1] + '/'
-    savePath = '../result/'+ sys.argv[1] + '/'
+    dataPath1 = '../data/'+ sys.argv[1] + '/' #path 1 of source files
+    dataPath2 = '../result/'+ sys.argv[1] + '/' #path 2 of source files
+    savePath = '../result/'+ sys.argv[1] + '/' #save path of result files
 
     s = getS(dataPath1+'LPI.csv')
     r_b =  getRank(dataPath2+'BC_score.csv')
